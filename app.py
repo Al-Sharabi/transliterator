c540b1a3292879a20b4836c6b14ef4cccfddf12a
Sharabi <Sharabi5@yandex.com>
2023-03-07 17:33:45 +0300

    transliteration_bot

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+import telebot
+from letters import letters
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN", default= None)
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+def welcome_func(message:telebot.types.Message):
+    bot.reply_to(message, 'Welcome')
+
+def transliter(message:telebot.types.Message):
+    he_result = ""
+    ar_result = ""
+    message_text = message.text.lower()
+    for letter in message_text:
+        if letter in list(letters.keys()):
+            he_result += letters[letter][0]
+            ar_result += letters[letter][1]
+        else:
+            he_result += letter
+            ar_result += letter
+    he_result = he_result.replace("s²", "שׁ").replace("s¹", "ס")
+    ar_result = ar_result.replace("s²", "ش").replace("s¹", "س")
+    bot.reply_to(
+        message=message, 
+        text=he_result)
+    bot.reply_to(
+        message=message, 
+        text=ar_result)
+
+
+bot.register_message_handler(commands=['start', 'help'], callback=welcome_func)
+bot.register_message_handler(func=lambda x:x, callback=transliter)
+if __name__ == "__main__":
+    bot.infinity_polling()
